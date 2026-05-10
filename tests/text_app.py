c60de23cd5ad97c42b7a1ecd239291ed97bc973d
nicmoraesnasc <nicmoraesnasc@email.com>
2026-05-10 16:34:27 -0300

    added continuous integration files

---
+++ tests/text_app.py
@@ -0,0 +1,18 @@
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'src', 'adornsaturn'))
+
+from app import app
+
+
+def test_login_page():
+    client = app.test_client()
+    response = client.get('/login')
+    assert response.status_code == 200
+
+
+def test_api_products():
+    client = app.test_client()
+    response = client.get('/api/products')
+    assert response.status_code == 200
